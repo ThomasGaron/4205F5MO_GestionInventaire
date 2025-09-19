87e73366acbdec99618688b1a06223ded4fa032f
FaridSani300 <farid.sani300@gmail.com>
2025-09-19 14:21:53 -0400

    [Add] fichier db.js pour la connexion à la base

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("supabase")
+    ? { rejectUnauthorized: false }
+    : undefined,
+});
+
+export default {
+  query: (texte, params) => pool.query(texte, params),
+};
