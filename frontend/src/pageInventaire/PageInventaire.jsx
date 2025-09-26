9096ebb4e4e29a8794a8095ce976bad41ff448e2
AntoineTaillon <2245749@cmontmorency.qc.ca>
2025-09-26 14:58:53 -0400

    [upd] : affichage simple dans la console des produits a partir de la base de donnees

--- frontend/src/pageInventaire/PageInventaire.jsx
+++ frontend/src/pageInventaire/PageInventaire.jsx
@@ -3,42 +3,60 @@ import "./PageInventaire.css";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth-context";
-import { useInventaire } from "../context/inventaire-context";
+// import { useInventaire } from "../context/inventaire-context";
 import { useAlert } from "../context/alert-context";
 import { getAllItems } from "../api/inventaireApi.jsx";
 import ItemCard from "../components/ItemCard";
 
 export default function PageInventaire() {
   const navigate = useNavigate();
-  const { isLoggedIn } = useContext(AuthContext);
-  const { items, setItems } = useInventaire();
-  const { setAlert } = useAlert();
+  // const { isLoggedIn } = useContext(AuthContext);
+  // const { items, setItems } = useInventaire();
+  // const { setAlert } = useAlert();
+
+  // useEffect(() => {
+  //   if (!isLoggedIn) {
+  //     navigate("/login");
+  //     return;
+  //   }
+
+  //   // Charger les items depuis l’API
+  //   getAllItems()
+  //     .then((data) => {
+  //       setItems(data);
+  //     })
+  //     .catch((err) => {
+  //       setAlert("Erreur lors du chargement des items : " + err.message);
+  //     });
+  // }, [isLoggedIn, navigate, setItems, setAlert]);
 
   useEffect(() => {
-    if (!isLoggedIn) {
-      navigate("/login");
-      return;
+    getAllItems();
+  }, []);
+  const getAllItems = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:5000/api/produit/tousLesProduits",
+        {
+          method: "GET",
+        }
+      );
+      const json = await res.json();
+      console.log(json);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-
-    // Charger les items depuis l’API
-    getAllItems()
-      .then((data) => {
-        setItems(data);
-      })
-      .catch((err) => {
-        setAlert("Erreur lors du chargement des items : " + err.message);
-      });
-  }, [isLoggedIn, navigate, setItems, setAlert]);
+  };
 
   return (
     <div className="page-inventaire">
       <h1>Inventaire</h1>
       <div className="items-grid">
-        {items && items.length > 0 ? (
+        {/* {items && items.length > 0 ? (
           items.map((item) => <ItemCard key={item.id} item={item} />)
         ) : (
           <p>Aucun item en stock</p>
-        )}
+        )} */}
       </div>
     </div>
   );
