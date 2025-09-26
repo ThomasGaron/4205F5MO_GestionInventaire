0f52b214655a3f708a6f7715ee02525175fdd2b1
AntoineTaillon <2245749@cmontmorency.qc.ca>
2025-09-26 16:12:56 -0400

    [upd] : manque un fichier

--- frontend/src/loginForm/LoginForm.jsx
+++ frontend/src/loginForm/LoginForm.jsx
@@ -25,11 +25,14 @@ export default function LoginForm() {
 
     try {
       // APPEL BACKEND
-      const response = await fetch("http://localhost:5000/api/user/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(connexion),
-      });
+      const response = await fetch(
+        import.meta.env.VITE_BACKEND_URI + "/api/user/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(connexion),
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Identifiants invalides");
