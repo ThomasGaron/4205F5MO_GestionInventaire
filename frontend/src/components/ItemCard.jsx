39bb5f9c7d4a0e72bc8804676f08755372c92068
ThomasGaron <thomasgaron@hotmail.ca>
2025-09-26 15:11:02 -0400

    [UPD] cartes des produits

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -3,18 +3,15 @@ import "./ItemCard.css";
 export default function ItemCard({ item }) {
   return (
     <div className="item-card">
-      <h3>{item.nom}</h3>
+      <h3>{item.produit_nom}</h3>
       <p>
         <strong>ID:</strong> {item.id}
       </p>
       <p>
-        <strong>Quantité:</strong> {item.quantite}
+        <strong>Prix:</strong> {item.produit_prix} $
       </p>
       <p>
-        <strong>Prix:</strong> {item.prix} $
-      </p>
-      <p>
-        <strong>Description:</strong> {item.description}
+        <strong>Quantité:</strong> {item.produit_quantiter}
       </p>
     </div>
   );
