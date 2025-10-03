2d892c1d86c49c5d69e4a75d50bc1ef5c3e79afc
ThomasGaron <thomasgaron@hotmail.ca>
2025-10-03 14:33:26 -0400

    [UPD] cartes de produit sans fonctionnalités

--- frontend/src/acceuil/Acceuil.jsx
+++ frontend/src/acceuil/Acceuil.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function Acceuil() {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>Bienvenue sur la Gestion d'Inventaire</h1>
-      <p>Ceci est la page d'accueil.</p>
+      <h1>Bienvenue sur le Gestionnaire d'Inventaire</h1>
+      <p>Gérez vos produits, vos stocks et vos employés dans la simplicité.</p>
     </div>
   );
 }
