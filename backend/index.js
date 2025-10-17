ce4b5a722f9ca1b6d2cc0a6f05d36d77df6ffcb5
ThomasGaron <thomasgaron@hotmail.ca>
2025-10-17 14:56:41 -0400

    [UPD] ajout des elements de facture dans index

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,17 @@ import userRoute from "./routes/user-routes.js";
 import produitRoute from "./routes/produit-routes.js";
 import invoiceRoute from "./routes/invoice-routes.js"; // factures
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
+
+// PDF facture
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +26,9 @@ app.use("/api/user", userRoute);
 app.use("/api/invoice", invoiceRoute);
 
 app.use("/api/produit", produitRoute);
+
+// Factures
+app.use("/invoices", express.static(path.join(__dirname, "invoices")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
