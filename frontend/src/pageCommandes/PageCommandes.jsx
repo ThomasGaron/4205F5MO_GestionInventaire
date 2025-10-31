0ee037f2d62f05b7f7efd74b8a624ca9cc62c1a8
AntoineTaillon <2245749@cmontmorency.qc.ca>
2025-10-31 11:27:45 -0400

    [upt] : affichage des noms des clients

--- frontend/src/pageCommandes/PageCommandes.jsx
+++ frontend/src/pageCommandes/PageCommandes.jsx
@@ -14,6 +14,8 @@ export default function PageCommandes() {
   const [detail, setDetail] = useState(null);
   const [updatingId, setUpdatingId] = useState(null);
 
+  const [nomClients, setNomClients] = useState([]);
+
   // Modal création
   const [openNew, setOpenNew] = useState(false);
   const [clients, setClients] = useState([]);
@@ -27,10 +29,10 @@ export default function PageCommandes() {
       navigate("/login");
       return;
     }
-    getAllCommandes();
+    getAllCommandesEtNomClient();
   }, [isLoggedIn, navigate]);
 
-  const getAllCommandes = async () => {
+  const getAllCommandesEtNomClient = async () => {
     try {
       setLoading(true);
       const res = await fetch(
@@ -45,6 +47,19 @@ export default function PageCommandes() {
       );
       const json = await res.json();
       setCommandes(json.data || []);
+
+      const reponse = await fetch(
+        import.meta.env.VITE_BACKEND_URI + "/api/clients",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const data = await reponse.json();
+      setNomClients(data.data);
     } catch (error) {
       console.error("Erreur chargement commandes:", error);
     } finally {
@@ -172,6 +187,13 @@ export default function PageCommandes() {
     }
   };
 
+  const getNomClient = (idClient) => {
+    const client = nomClients.find((cl) => cl.id === idClient);
+    return client
+      ? `${client.client_prenom} ${client.client_nom}`
+      : "Client inconnu";
+  };
+
   const closeModal = () => {
     setOpenNew(false);
     setClientId("");
@@ -245,7 +267,7 @@ export default function PageCommandes() {
       const json = await res.json();
       if (!res.ok) throw new Error(json?.error || "Erreur création commande.");
 
-      await getAllCommandes();
+      await getAllCommandesEtNomClient();
       setDetail(null);
       closeModal();
 
@@ -491,8 +513,14 @@ export default function PageCommandes() {
 
               <div className="commande-infos">
                 <p>
-                  <strong>Client:</strong> {c.client_id}
+                  <strong>Client id:</strong> {c.client_id}
                 </p>
+
+                <p>
+                  <strong>Client nom:</strong>
+                  {getNomClient(c.client_id)}
+                </p>
+
                 {"date" in c && (
                   <p>
                     <strong>Date:</strong> {c.date}
