2260de3665ffe0af970b330a9dc203e1ce33b658
FaridSani300 <farid.sani300@gmail.com>
2025-11-21 13:10:12 -0500

    [Upd] modification du fichier vite.config.js pour les configuration des tests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    globals: true,
+    css: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+      all: true,
+      include: [
+        "src/components/**/*.{js,jsx,ts,tsx}",
+        "src/page*/**/*.{js,jsx,ts,tsx}",
+        "src/lib/**/*.{js,jsx,ts,tsx}",
+      ],
+      exclude: [
+        "**/*.{test,spec}.{js,jsx,ts,tsx}",
+        "**/__tests__/**",
+        "node_modules/**",
+        "dist/**",
+        "coverage/**",
+      ],
+    },
+  },
+});
