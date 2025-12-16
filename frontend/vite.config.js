0c7ceb0dc57fbef88a5fde5f1e3bcb9cdf008076
TaillonAntoine <2245749@cmontmorency.qc.ca>
2025-12-16 14:58:07 -0500

    [fix] : manque ] dans le fichier vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,25 +4,27 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-environment: "jsdom",
-  globals: true,
-  setupFiles: "./vitest.setup.ts",
-  css: true,
-  coverage: {
-    provider: "v8",
-    reporter: ["text", "html", "lcov"],
-    reportsDirectory: "./coverage",
-    all: true,
-    include: [
-      "src/components/**/*.{js,jsx,ts,tsx}",
-      "src/page*/**/*.{js,jsx,ts,tsx}",
-      "src/lib/**/*.{js,jsx,ts,tsx}",
-    ],
-    exclude: [
-      "**/*.{test,spec}.{js,jsx,ts,tsx}",
-      "**/__tests__/**",
-      "node_modules/**",
-      "dist/**",
-      "coverage/**",
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./vitest.setup.ts",
+    css: true,
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+      all: true,
+      include: [
+        "src/components/**/*.{js,jsx,ts,tsx}",
+        "src/page*/**/*.{js,jsx,ts,tsx}",
+        "src/lib/**/*.{js,jsx,ts,tsx}",
+      ],
+      exclude: [
+        "**/*.{test,spec}.{js,jsx,ts,tsx}",
+        "**/__tests__/**",
+        "node_modules/**",
+        "dist/**",
+        "coverage/**",
+      ],
+    },
   },
 });
